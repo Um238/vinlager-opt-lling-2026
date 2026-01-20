d95cb60f39c4390c844b9b0bbf13e126456546d9
Um238 <mikkelsen67@outlook.dk>
2026-01-20 15:10:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Backend konfiguration
 // SIMPEL: Brug altid localhost når du kører lokalt
 const CONFIG = {
-  API_URL: 'http://localhost:3000',  // ÆNDR KUN DENNE HVIS BACKEND ER I CLOUD
+  API_URL: '   API_URL: 'https://vinlager-opt-lling-2026.onrender.com',',  // ÆNDR KUN DENNE HVIS BACKEND ER I CLOUD
   TIMEOUT: 10000
 };
+
