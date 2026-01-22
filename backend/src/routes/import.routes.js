d4dd3d67db273c69f354f2131723d7bc50fdfb93
Um238 <mikkelsen67@outlook.dk>
2026-01-22 17:06:43 +0100

    Update import.routes.js
    Tilføj export routes

--- backend/src/routes/import.routes.js
+++ backend/src/routes/import.routes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const importController = require('../controllers/import.controller');
 const { importUpload } = require('../utils/upload');
 
+// Import routes
 router.post('/csv', importUpload.single('file'), importController.importCSV);
 router.post('/excel', importUpload.single('file'), importController.importExcel);
+
+// Export routes
+router.get('/csv', importController.exportCSV);
+router.get('/excel', importController.exportExcel);
 
 module.exports = router;
