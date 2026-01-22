8487827db02431b1d218de34cba83da4c441c7ce
Um238 <mikkelsen67@outlook.dk>
2026-01-22 08:16:11 +0100

    Add save and history routes for reports

--- backend/src/routes/reports.routes.js
+++ backend/src/routes/reports.routes.js
@@ -4,5 +4,7 @@ const reportsController = require('../controllers/reports.controller');
 
 router.get('/lager', reportsController.getLagerReport);
 router.get('/værdi', reportsController.getVærdiReport);
-
+router.post('/save', reportsController.saveReport);
+router.get('/history', reportsController.getReportsHistory);
 module.exports = router;
+
